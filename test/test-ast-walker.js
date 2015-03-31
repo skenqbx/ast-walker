53c51136a27b0c769c5816f0fdc03c6df50cfcf0
Malte-Thorben Bruns <skenqbx@gmail.com>
2015-04-01 01:22:11 +0200

    wip

---
+++ test/test-ast-walker.js
@@ -0,0 +1,19 @@
+'use strict';
+/* global suite: false, setup: false, test: false,
+    teardown: false, suiteSetup: false, suiteTeardown: false */
+var assert = require('assert');
+var ASTWalker = require('../');
+
+
+suite('ast-walker', function() {
+
+  suiteSetup(function(done) {
+    done();
+  });
+
+  test('dummy', function() {});
+
+  suiteTeardown(function(done) {
+    done();
+  });
+});
